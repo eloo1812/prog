505bd5e4604fddade6b844ce05f16715a72b22ef
eloo1812 <142434356+eloo1812@users.noreply.github.com>
2024-12-01 22:50:56 -0300

    commit

---
+++ src/main/java/com/example/springboot/repositories /repositories /ProdutoRepository.java
@@ -0,0 +1,13 @@
+package com.example.springboot.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.springboot.models.ProdutoModel;
+
+import java.util.UUID;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<ProdutoModel, UUID> {
+
+}
